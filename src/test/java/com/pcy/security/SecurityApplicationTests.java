e22eb40f626d8be41479266a5ddff33b6c39f842
박찬영 <pcy@bagchan-yeong-ui-MacBookAir.local>
2021-02-12 19:13:07 +0900

    스프링부트 OAuth2.0을 위한 기본 세팅 완료

---
+++ src/test/java/com/pcy/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcy.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
